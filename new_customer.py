f3f182865c73bacfdd8084ba940cdc594f196605
100878550 <ethan.smallwood@dcmail.ca>
2025-04-14 17:58:58 -0400

    first commit

---
+++ new_customer.py
@@ -0,0 +1,41 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+
+class testNewCustomer(unittest.TestCase):
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome()
+        cls.driver.maximize_window()
+        cls.driver.get("http://demo.guru99.com/V4/")
+        
+    
+    def teardown_method(cls):
+        cls.driver.quit()
+    
+
+    def test_NC1_verify_name(self):
+        driver = self.driver
+        driver.find_element(By.NAME, "uid").send_keys("mngr123456")
+        driver.find_element(By.NAME, "password").send_keys("password123")
+        driver.find_element(By.NAME, "btnLogin").click()
+        
+        # Wait for the page to load and the element to be present
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, "New Customer"))
+        )
+        
+        driver.find_element(By.LINK_TEXT, "New Customer").click()
+        
+        # Verify the presence of the 'Customer Name' field
+        assert driver.find_element(By.NAME, "name").is_displayed(), "Customer Name field is not displayed"
+        assert driver.find_element(By.NAME, "name").is_enabled(), "Customer Name field is not enabled"
+
+if __name__ == "__main__":
+    suite = unittest.TestSuite()
+    suite.addTest(testNewCustomer("test_NC1_verify_name"))
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
